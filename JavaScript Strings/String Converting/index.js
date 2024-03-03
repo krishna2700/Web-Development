fa474d8ba727f93531244da936af33bd3da494c3
krishna2700 <krishnaruparelia0207@gmail.com>
2024-03-03 14:15:09 +0530

    Strings completed

--- JavaScript Strings/String Converting/index.js
+++ JavaScript Strings/String Converting/index.js
@@ -47,3 +47,56 @@ const bolleanString = true;
 const convertIt = bolleanString.toString();
 console.log(typeof bolleanString);
 console.log(typeof convertIt);
+
+//  Converting from strings to Numbers
+
+// converting pixel value from Number with parseInt()
+
+const pixelValue = "42px";
+console.log(typeof pixelValue);
+const CovertNumber = parseInt(pixelValue);
+console.log(typeof CovertNumber);
+console.log(CovertNumber + 10);
+
+//  Converting dimention String to Number with parseFloat()
+
+const dimentionSt = "42.5px";
+const dimentionConver = parseFloat(dimentionSt);
+console.log(typeof dimentionConver);
+console.log(dimentionConver + 7.5);
+
+// Convert age Input to Number using number()
+
+const ageInput = "42";
+const ConvertAge = Number(ageInput);
+console.log(typeof ConvertAge);
+console.log(typeof ageInput);
+console.log(ConvertAge * 9);
+console.log(ageInput + 9);
+
+//  Quick Convertion using unary + Operator
+
+const StoreString = "100";
+const convertedStore = +StoreString;
+console.log(typeof convertedStore);
+console.log(convertedStore + 100);
+
+//  Converting from String to booleans
+
+// Using Boolean() for form validation
+
+const userInput = "Krishna";
+const userInput1 = "";
+const ConvertedBoolean = Boolean(userInput);
+const ConvertedBoolean1 = Boolean(userInput1);
+console.log(ConvertedBoolean);
+console.log(ConvertedBoolean1);
+
+// using Double Negation !! for Quick Convertion
+
+const EmailInput = "Krishna@gmail.com";
+const EmailInput1 = "";
+const QuickNegation = !!EmailInput;
+const QuickNegation1 = !!EmailInput1;
+console.log(QuickNegation);
+console.log(QuickNegation1);
